8bbcee44a3320478d3566465beb57303287b6c21
kurcinamarian <marian.kurcina2003@gmail.com>
2024-11-27 23:30:25 +0100

    done

--- Backpropagation algoritmus.py
+++ Backpropagation algoritmus.py
@@ -1,134 +1,213 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-np.random.seed(42)
+#random seed for testing
+np.random.seed(43)
 
 class Model:
     def __init__(self, layers):
         self.layers = layers
 
+    #for layers compute its forward function and use output as input for next layer
     def forward(self, x):
         for layer in self.layers:
             x = layer.forward(x)
+        #output of the model (prediction)
         return x
 
+    #for layers in reversed compute gradiant and use it as input for the next layer
     def backward(self, grad):
         for layer in reversed(self.layers):
             grad = layer.backward(grad)
 
-
+#Linear layer
 class Linear:
     def __init__(self, input_size, output_size):
+        #weights matrix
         self.W = np.random.randn(input_size, output_size)
+        #bias matrix
         self.b = np.random.randn(output_size)
+        #gradient weights matrix
         self.dW = None
+        #gradient bias matrix
         self.db = None
+        #if momentum add momentum related variables
         if momentum:
-            self.momentum = 0.9
+            #momentum rate
+            self.momentum = momentum_rate
+            #weight velocity matrix
             self.vW = np.zeros_like(self.W)
+            #bias velocity matrix
             self.vb = np.zeros_like(self.b)
 
+    #matrix multiplication of input and weight matrix and adding biases
     def forward(self, x):
         self.input = x
         return np.dot(x, self.W) + self.b
 
+
     def backward(self, grad):
+        #Reshape of input
         self.input = self.input.reshape(1, -1)
+        #Reshape of gradient
         grad = grad.reshape(1, -1)
 
+        #compute gradient weights matrix
         self.dW = np.dot(self.input.T, grad)
+        #compute gradient bias matrix
         self.db = np.sum(grad, axis=0)
+        #if momentum update acoring to momentum rate
         if momentum:
+            #compute weight velocity matrix
             self.vW = self.momentum*self.vW - learning_rate * self.dW
+            #compute bias velocity matrix
             self.vb = self.momentum*self.vb - learning_rate * self.db
+            #change weight matrix according to velocity
             self.W += self.vW
+            #change bias matrix according to velocity
             self.b += self.vb
         else:
+            #change weight matrix according to learning rate
             self.W -= learning_rate * self.dW
+            #change bias matrix according to velocity
             self.b -= learning_rate * self.db
+        #return gradient for previous layer
         return np.dot(grad, self.W.T)
 
-
+#Sigmoid activation function
 class Sigmoid:
     def __init__(self):
         self.output = None
 
+    #Sigmoid function
     def forward(self, x):
         self.output = 1 / (1 + np.exp(-x))
         return self.output
 
+    #Sigmoid backward function
     def backward(self, grad):
+        #return gradient for previous layer
         return grad * self.output * (1 - self.output)
 
-
+#Tanh activation function
 class Tanh:
     def __init__(self):
         self.output = None
 
+    #Tanh function
     def forward(self, x):
-        self.input = x
         self.output = np.tanh(x)
         return self.output
 
+    #Tanh backward function
     def backward(self, grad):
+        #return gradient for previous layer
         return grad * (1 - self.output ** 2)
 
-
+#RELU activation function (if larger the 0 output is x otherwise 0)
 class Relu:
     def __init__(self):
         self.input = None
 
+    #Relu function
     def forward(self, x):
         self.input = x
         return np.maximum(0, x)
 
+    #Relu backward function
     def backward(self, grad):
+        #return gradient for previous layer
         return grad * (self.input > 0)
 
-
+#MSELoss (Mean Squared Error Loss)
 class MSELoss:
     def __init__(self):
         self.predicted = None
         self.target = None
 
+    #return average difference squared for predictions and true values
     def forward(self, predicted, target):
         self.predicted = predicted
         self.target = target
         return np.mean((predicted - target) ** 2)
 
+    #derivated MSELoss function
     def backward(self):
         return 2 * (self.predicted - self.target) / self.predicted.size
 
+########################################################################################################################
+#input for testing different parameters
+learning_rate = float(input("Learning rate: "))
+epoch_number = int(input("Number of epochs: "))
+momentum = input("Momentum(y/n): ")
+if momentum == "y":
+    momentum = True
+    momentum_rate = float(input("Momentum: "))
+else:
+    momentum = False
+function = input("Function: ")
 
-
-input_data = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
-predicted_output = np.array([[0], [1], [1], [0]])
-
+#definition of network model and MSELoss
+model = Model([
+    Linear(2, 4),
+    Tanh(),
+    Linear(4, 1),
+    Sigmoid()
+])
 loss_fn = MSELoss()
-learning_rate = 0.1
-epoch_number = 50000
-momentum = False
 
-model = Model([Linear(2, 4), Tanh(), Linear(4, 1), Tanh()])
+#definition of inputs and predicted outputs
+input_data = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+if function == 'XOR':
+    output_data = np.array([[0], [1], [1], [0]])
+elif function == 'OR':
+    output_data = np.array([[0], [1], [1], [1]])
+elif function == 'AND':
+    output_data = np.array([[0], [0], [0], [1]])
+else:
+    print("Invalid function")
+    exit()
 
+data = np.hstack((input_data, output_data))
+
+#training process
 losses = []
-for epoch in range(epoch_number):
+for epoch in range(1,epoch_number+1):
     epoch_loss = 0
-    for i in range(len(input_data)):
+    #shuffle data
+    np.random.shuffle(data)
+    input_data = data[:, :2]
+    output_data = data[:, 2:]
+    #try all inputs
+    for i in range(4):
+        #get predicted result by running input forward
         prediction = model.forward(input_data[i])
-
-        loss = loss_fn.forward(prediction, predicted_output[i])
-        losses.append(loss)
+        #get MSELoss (difference) between prediction and real output for the input
+        loss = loss_fn.forward(prediction, output_data[i])
+        #get gradient based on the model's prediction
         grad = loss_fn.backward()
+        #go backward and change linear layers (weights and bias)
         model.backward(grad)
-
         epoch_loss += loss
-
+    #get average loss for inputs and add to losses list
+    avg_epoch_loss = epoch_loss / 4
+    losses.append(avg_epoch_loss)
     if epoch % 10 == 0:
-        print(f'Epoch {epoch}, Loss {epoch_loss}')
+        #average loss for epoch
+        print(f'Epoch: {epoch}, Loss: {avg_epoch_loss/4}')
 
-print(f'Epoch {epoch}, Loss {loss}')
-for i in range(len(input_data)):
-    prediction = model.forward(input_data[i])
-    print(input_data[i], prediction)
+#test of the model
+for i in [[0, 0], [0, 1], [1, 0], [1, 1]]:
+    #run model forward and get model's prediction
+    prediction = model.forward(i)
+    print(f"Input: {i} Predicted output: {prediction}")
 
+#plot the losses over epochs
+plt.plot(range(1, epoch_number + 1), losses, label='Training Loss', color='blue', marker='o',markersize=1)
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.title(function)
+plt.legend(loc='upper right')
+plt.grid(True)
+plt.show()
 
--- MNIST.py
+++ MNIST.py
@@ -3,95 +3,176 @@ import torch.nn as nn
 import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
 
-batch_size = 128
+#number of epochs
 num_epochs = 30
-device = torch.device("cpu")
 
-transform = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize((0.5,), (0.5,))
-])
-
-train_dataset = datasets.MNIST(root="./data", train=True, transform=transform, download=True)
-test_dataset = datasets.MNIST(root="./data", train=False, transform=transform, download=True)
-
-train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
-
-
+#model definition
 class MLP(nn.Module):
     def __init__(self):
         super(MLP, self).__init__()
         self.layers = nn.Sequential(
+            #making 28*28 into vector
             nn.Flatten(),
-            nn.Linear(28 * 28, 1000),
+            #first layer (input to 100)
+            nn.Linear(28 * 28, 100),
+            #relu activation function
             nn.ReLU(),
-            nn.Linear(1000, 500),
+            #second layer (100 to 50)
+            nn.Linear(100, 50),
+            #relu activation function
             nn.ReLU(),
-            nn.Linear(500, 10),
+            #third layer (50 to output)
+            nn.Linear(50, 10),
         )
 
+    #gets predicted output from running input through layers
     def forward(self, x):
         return self.layers(x)
 
-
-def train_model(optimizer_type):
-    model = MLP().to(device)
+#training the model
+def train_model(optimizer_type, learning_rate, momentum):
+    #create model
+    model = MLP()
+    #from output creates probabilities of items
     criterion = nn.CrossEntropyLoss()
 
+    #optimizer type
     if optimizer_type == "SGD":
-        optimizer = optim.SGD(model.parameters(), lr=0.05)
+        optimizer = optim.SGD(model.parameters(), learning_rate)
     elif optimizer_type == "SGD_momentum":
-        optimizer = optim.SGD(model.parameters(), lr=0.05, momentum=0.9)
+        optimizer = optim.SGD(model.parameters(), learning_rate, momentum)
     elif optimizer_type == "ADAM":
-        optimizer = optim.Adam(model.parameters(), lr=0.001)
+        optimizer = optim.Adam(model.parameters(), learning_rate)
     else:
         raise ValueError("Unknown optimizer type")
-
+    # Lists to store losses and accuracies
+    train_losses = []
+    test_losses = []
+    test_accuracies = []
     for epoch in range(num_epochs):
+        #sets model to training
         model.train()
-        running_loss = 0.0
+        running_train_loss = 0.0
+        #trains for all images in training dataset
         for images, labels in train_loader:
-            images, labels = images.to(device), labels.to(device)
-
+            #resets gradients
             optimizer.zero_grad()
+            #model's prediction
             outputs = model(images)
+            #difference between prediction and true outputs
             loss = criterion(outputs, labels)
+            #gradients computing
             loss.backward()
+            #updating weights based on optimizer
             optimizer.step()
-
-            running_loss += loss.item()
-
-        print(f"{optimizer_type} - Epoch {epoch + 1}, Loss: {running_loss / len(train_loader):.4f}")
-
+            running_train_loss += loss.item()
+        avg_train_loss = running_train_loss / len(train_loader)
+        train_losses.append(avg_train_loss)
+        #test model
+        model.eval()
+        running_test_loss = 0.0
+        correct_test = 0
+        total_test = 0
+        with torch.no_grad():
+            for images, labels in test_loader:
+                outputs = model(images)
+                loss = criterion(outputs, labels)
+                running_test_loss += loss.item()
+                _, predicted = torch.max(outputs, 1)
+                total_test += labels.size(0)
+                correct_test += (predicted == labels).sum().item()
+        #compute variables needed for graph
+        avg_test_loss = running_test_loss / len(test_loader)
+        test_losses.append(avg_test_loss)
+        test_accuracy = 100 * correct_test / total_test
+        test_accuracies.append(test_accuracy)
+        print(f"{optimizer_type} - Epoch {epoch + 1}, Loss: {avg_train_loss:.4f}")
+    #create graph
+    fig, ax1 = plt.subplots(figsize=(10, 6))
+    #training loss
+    ax1.plot(range(1, num_epochs + 1), train_losses, label='Training Loss', color='blue', marker='o')
+    #testing loss
+    ax1.plot(range(1, num_epochs + 1), test_losses, label='Test Loss', color='red', marker='o')
+    ax1.set_xlabel('Epochs')
+    ax1.set_ylabel('Loss')
+    ax1.set_title(f'Training and Test Loss - {optimizer_type}')
+    ax1.legend(loc='upper left')
+    ax1.grid(True)
+    #second axis
+    ax2 = ax1.twinx()
+    #accuracies
+    ax2.plot(range(1, num_epochs + 1), test_accuracies, label='Test Accuracy', color='orange', marker='x')
+    ax2.set_ylabel('Accuracy (%)')
+    ax2.legend(loc='lower right')
+    plt.show()
+    #return trained model
     return model
 
-
-def evaluate_model(model):
+#testing the model
+def test_model(model):
+    #sets model to evaluation mode
     model.eval()
     correct = 0
     total = 0
+    #confusion matrix ()
     matrix = [[0 for _ in range(10)] for _ in range(10)]
+    #disables gradient computing
     with torch.no_grad():
+        #tests for all images in test dataset
         for images, labels in test_loader:
-            images, labels = images.to(device), labels.to(device)
+            #model's prediction
             outputs = model(images)
+            #finds max in output that being the predicted number
             _, predicted = torch.max(outputs.data, 1)
+            #adds number of data in batch
             total += labels.size(0)
+            #for images in batches add number of correctly labeled
             correct += (predicted == labels).sum().item()
+            #update of the confusion matrix (rows are true labels columns are predicted labels)
             for p, t in zip(predicted, labels):
                 matrix[t.item()][p.item()] += 1
-
     accuracy = 100 * correct / total
     print(f"Test Accuracy: {accuracy:.2f}%")
-    for row in matrix:
-        print(row)
+    #print confusion matrix
+    print("Confusion Matrix:")
+    print("Predicted->  " + "  ".join(f"{i:>5}" for i in range(10)))
+    print("------------------------------------------------------------------------------------------------------------------")
+    #print rows with true values on left
+    for i, row in enumerate(matrix):
+        print(f"\t\t{i:<3} |", "  ".join(f"{x:5}" for x in row))
     return accuracy
 
+#transformation of data
+transform = transforms.Compose([
+    transforms.ToTensor(),
+])
 
+#loads train data and transform them
+train_dataset = datasets.MNIST(root="./data", train=True, transform=transform, download=True)
+#loads test data and transform them
+test_dataset = datasets.MNIST(root="./data", train=False, transform=transform, download=True)
 
-for optimizer_type in ["SGD", "SGD_momentum", "ADAM"]:
-    print(f"\nTraining with {optimizer_type}")
-    trained_model = train_model(optimizer_type)
-    test_accuracy = evaluate_model(trained_model)
+#splits data into batches and shuffles them
+train_loader = DataLoader(train_dataset, batch_size=128, shuffle=True)
+#splits data into batches
+test_loader = DataLoader(test_dataset, batch_size=128, shuffle=False)
+
+#tries to train and then test model for optimization types
+
+print(f"\nTraining with SGD")
+#train model with SGD
+model = train_model("SGD",0.1,0.9)
+#test model
+test_accuracy_SGD = test_model(model)
+print(f"\nTraining with SGD_momentum")
+# train model with SGD_momentum
+model = train_model("SGD_momentum", 0.1, 0.9)
+# test model
+test_accuracy_SGD_momentum = test_model(model)
+print(f"\nTraining with ADAM")
+# train model with ADAM
+model = train_model("ADAM", 0.01, 0.9)
+# test model
+test_accuracy_ADAM = test_model(model)
