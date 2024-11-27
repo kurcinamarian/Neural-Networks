b0213907d47afa3b8552500ba2b4b3f5ba189b80
kurcinamarian <marian.kurcina2003@gmail.com>
2024-11-27 01:26:56 +0100

    second verz

--- Backpropagation algoritmus.py
+++ Backpropagation algoritmus.py
@@ -1,34 +1,121 @@
 import numpy as np
-import math
 
 class Model:
+    def __init__(self, layers):
+        self.layers = layers
+
+    def forward(self, x):
+        for layer in self.layers:
+            x = layer.forward(x)
+        return x
+
+    def backward(self, grad):
+        for layer in reversed(self.layers):
+            grad = layer.backward(grad)
+
+
+class Linear:
+    def __init__(self, input_size, output_size):
+        self.W = np.random.randn(input_size, output_size)
+        self.b = np.random.randn(output_size)
+        self.dW = None
+        self.db = None
+
+    def forward(self, x):
+        self.input = x
+        return np.dot(x, self.W) + self.b
+
+    def backward(self, grad):
+        self.input = self.input.reshape(1, -1)
+        grad = grad.reshape(1, -1)
+
+        self.dW = np.dot(self.input.T, grad)
+        self.db = np.sum(grad, axis=0)
+
+        self.W -= learning_rate * self.dW
+        self.b -= learning_rate * self.db
+
+        return np.dot(grad, self.W.T)
+
+
+class Sigmoid:
     def __init__(self):
-        self.linear1 = np.random.rand(2, 4)
-        self.cons1 = np.random.rand(1, 4)
-        self.linear2 = np.random.rand(4, 1)
+        self.output = None
 
-    def tanh(self, input):
-        return [(math.exp(x) - math.exp(-x)) / (math.exp(x) + math.exp(-x)) for x in input]
+    def forward(self, x):
+        self.input = x
+        self.output = 1 / (1 + np.exp(-x))
+        return self.output
 
-    def sigmoid(self, input):
-        return 1 / (1 + np.exp(-input))
-
-    def relu(self, input):
-        return np.maximum(0, input)
-
-    def forward(self, input):
-        input = np.array(input).reshape(1, -1)
-        input = np.dot(input, self.linear1)
-        input += self.cons1
-        input = self.tanh(input)
-        input = np.dot(input, self.linear2)
-        return input
+    def backward(self, grad):
+        return grad * self.output * (1 - self.output)
 
 
-model = Model()
+class Tanh:
+    def __init__(self):
+        self.output = None
 
-input_data = [[0, 0], [0, 1], [1, 0], [1, 1]]
+    def forward(self, x):
+        self.input = x
+        self.output = np.tanh(x)
+        return self.output
 
-for i in input_data:
-    output = model.forward(i)
-    print("Output:", output)
+    def backward(self, grad):
+        return grad * (1 - self.output ** 2)
+
+
+class Relu:
+    def __init__(self):
+        self.input = None
+
+    def forward(self, x):
+        self.input = x
+        return np.maximum(0, x)
+
+    def backward(self, grad):
+        return grad * (self.input > 0)
+
+
+class MSELoss:
+    def __init__(self):
+        self.predicted = None
+        self.target = None
+
+    def forward(self, predicted, target):
+        self.predicted = predicted
+        self.target = target
+        return np.mean((predicted - target) ** 2)
+
+    def backward(self):
+        return 2 * (self.predicted - self.target) / self.predicted.size
+
+
+model = Model([Linear(2, 4), Tanh(), Linear(4, 1), Sigmoid()])
+
+input_data = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+predicted_output = np.array([[0], [1], [1], [0]])
+
+loss_fn = MSELoss()
+learning_rate = 0.1
+epoch_number = 500
+
+losses = []
+for epoch in range(epoch_number):
+    for i in range(len(input_data)):
+        prediction = model.forward(input_data[i])
+
+        loss = loss_fn.forward(prediction, predicted_output[i])
+        losses.append(loss)
+
+        grad = loss_fn.backward()
+        model.backward(grad)
+
+    if epoch % 10 == 0:
+        print(f'Epoch {epoch}, Loss {loss}')
+
+print(f'Epoch {epoch}, Loss {loss}')
+for i in range(len(input_data)):
+    prediction = model.forward(input_data[i])
+    print(input_data[i], prediction)
+
+
